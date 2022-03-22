404e1892f9733729564ad537edeb7f3977579a9f
slime830 <ossar830@gmail.com>
2022-03-22 14:07:55 +0900

    20220322

--- chapter3.py
+++ chapter3.py
@@ -1,5 +1,7 @@
 import json
 import re
+from urllib import request
+import requests
 
 title_column_name = "title"
 text_column_name = "text"
@@ -9,6 +11,9 @@ def print_separater(problem_num):
     print("-" * 10)
     print(f"problem{problem_num}")
 
+def print_dict(dict:dict):
+    for key,value in dict.items():
+        print(key+" + "+value)
 
 def problem20(word="イギリス"):
     with open("jawiki-country.json", "r", encoding="utf-8") as f:
@@ -64,6 +69,28 @@ def problem27(document, template_dict):
         for key, value in template_dict.items()
     }
 
+def problem28(document,template_dict):
+    return {
+        key:
+        re.sub(
+            r'\{\{(?:lang|仮リンク)(?:[^|]*?\|)*?([^|]*?)\}\}' , r'\1',
+            re.sub(
+                r'<.+?>',"",
+                re.sub(
+                    r'https?://[\w!?/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+',"",
+                    re.sub(
+                        r"\[\[(?:[^|]*?\|)??([^|]*?)\]\]", r"\1", re.sub(r"\'{2,5}", "", value)
+                    )
+                )
+            )
+        )
+        for key,value in template_dict.items()
+    }
+
+def problem29(template_dict):
+    url="https://commons.wikimedia.org/w/api.php?action=query&titles=File:" + template_dict.get("国旗画像").replace(" ","_") + '&prop=imageinfo&iiprop=url&format=json'
+    data = requests.get(url)
+    return re.search(r"\"url\":\"(.+?)\"",data.text).group(1)
 
 def main():
     print_separater(20)
@@ -79,12 +106,16 @@ def main():
     print(problem24(uk_document))
     print_separater(25)
     template_dict = problem25(uk_document)
-    print(uk_document, template_dict)
+    print_dict(template_dict)
     print_separater(26)
-    print(problem26(uk_document, template_dict))
+    print_dict(problem26(uk_document, template_dict))
     print_separater(27)
-    print(problem27(uk_document, template_dict))
-
+    print_dict(problem27(uk_document, template_dict))
+    print_separater(28)
+    print_dict(problem28(uk_document,template_dict))
+    print_separater(29)
+    print(problem29(template_dict))
+    
 
 if __name__ == "__main__":
     main()
